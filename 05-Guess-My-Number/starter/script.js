f08202296d7f6c1204fb818ee9344c3260c1d818
uhohjackieson <109990923+uhohjackieson@users.noreply.github.com>
2023-10-31 15:39:13 -0400

    added event listener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,11 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
-console.log('hi')
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 15;
+// document.querySelector('.score');
+
+// document.querySelector('.guess').value = 23;
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
